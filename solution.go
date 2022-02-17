806846ef7094cf6433d669472176fc678bd06b8e
vipos89 <1989146ViPoS>
2022-02-17 17:39:26 +0300

    ololo

--- solution.go
+++ solution.go
@@ -23,7 +23,7 @@ func CalcSquare(sideLen float64, sidesNum myInt) float64 {
 	case 0:
 		return math.Pi * math.Pow(sideLen, 2)
 	case 3:
-		return (math.Sqrt(3) / 4) * math.Pow(sideLen, 2)
+		return math.Sqrt(3) * math.Pow(sideLen, 2) / 4
 	case 4:
 		return math.Pow(sideLen, 2)
 	default:
